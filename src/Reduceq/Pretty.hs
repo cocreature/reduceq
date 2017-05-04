cac535318e0c7969f8cc1d39329ef91bc484b63a
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2017-05-04 12:22:14 +0200

    Support new version of wl-pprint with annotated docs

--- src/Reduceq/Pretty.hs
+++ src/Reduceq/Pretty.hs
@@ -11,62 +11,62 @@ import           Reduceq.Prelude
 
 import qualified Data.Map as Map
 import           Data.Text.Prettyprint.Doc hiding ((<>))
+import           Data.Text.Prettyprint.Doc.Render.Terminal (Color(..), color, AnsiTerminal)
 import           Data.Text.Prettyprint.Doc.Render.Text
 
 import           Reduceq.CoqAST
 
-type VarColors = Map VarId SColor
+type VarColors = Map VarId Color
 
-newtype PprintM a = PprintM (StateT SColor (Reader VarColors) a)
-  deriving (Functor, Applicative, Monad, MonadState SColor, MonadReader VarColors)
+newtype PprintM a = PprintM (StateT Color (Reader VarColors) a)
+  deriving (Functor, Applicative, Monad, MonadState Color, MonadReader VarColors)
 
 runPprintM :: PprintM a -> a
 runPprintM (PprintM a) =
-  runReader (evalStateT a SRed) Map.empty
+  runReader (evalStateT a Red) Map.empty
 
 shiftVarMap :: VarColors -> VarColors
 shiftVarMap = Map.mapKeys succ
 
-withBoundVar :: (SColor -> PprintM a) -> PprintM a
+withBoundVar :: (Color -> PprintM a) -> PprintM a
 withBoundVar x = do
   c <- get
   put (nextColor c)
   local (Map.insert (VarId 0) c . shiftVarMap) (x c)
 
-coloredVar :: VarId -> PprintM Doc
-
+coloredVar :: VarId -> PprintM (Doc AnsiTerminal)
 coloredVar id@(VarId index) = do
   c <- asks (Map.lookup id)
   case c of
     Nothing -> panic ("Unknown variable index: " <> show id)
     Just c' -> (pure . color c' . pretty @Text) ("v" <> show index)
 
-nextColor :: SColor -> SColor
-nextColor SRed = SGreen
-nextColor SGreen = SYellow
-nextColor SYellow = SBlue
-nextColor SBlue = SMagenta
-nextColor SMagenta = SCyan
-nextColor SCyan = SRed
+nextColor :: Color -> Color
+nextColor Red = Green
+nextColor Green = Yellow
+nextColor Yellow = Blue
+nextColor Blue = Magenta
+nextColor Magenta = Cyan
+nextColor Cyan = Red
 nextColor _ = panic "Can only cycle between visible colors"
 
-pprintTy :: Ty -> Doc
+pprintTy :: Ty -> Doc a
 pprintTy TyInt = "Int"
 pprintTy TyBool = "Bool"
 pprintTy TyUnit = "()"
 pprintTy (TyProd x y) = pprintTy x <+> "*" <+> pprintTy y
 
-pprintOp :: IntBinop -> Doc
+pprintOp :: IntBinop -> Doc a
 pprintOp IAdd = "+"
 pprintOp IMul = "*"
 pprintOp ISub = "-"
 
-pprintComp :: IntComp -> Doc
+pprintComp :: IntComp -> Doc a
 pprintComp IEq = "="
 pprintComp ILt = "<"
 pprintComp IGt = ">"
 
-pprintExpr :: Expr -> PprintM Doc
+pprintExpr :: Expr -> PprintM (Doc AnsiTerminal)
 pprintExpr (Var id) = coloredVar id
 pprintExpr (IntLit i)
   | i >= 0 = pure (pretty i)
@@ -99,10 +99,10 @@ pprintExpr (Inl x) = parens . ("inl" <+>) <$> pprintExpr x
 pprintExpr (Inr x) = parens . ("inr" <+>) <$> pprintExpr x
 pprintExpr Unit = pure "()"
 
-displayDoc :: Doc -> Text
-displayDoc = renderStrict . layoutPretty defaultLayoutOptions
+displayDoc :: Doc a -> Text
+displayDoc = renderStrict . layoutPretty defaultLayoutOptions . unAnnotate
 
-displayCompact :: Doc -> Text
+displayCompact :: Doc a -> Text
 displayCompact =
   renderStrict .
-  layoutPretty (defaultLayoutOptions {layoutPageWidth = Unbounded})
+  layoutPretty (defaultLayoutOptions {layoutPageWidth = Unbounded}) . unAnnotate
