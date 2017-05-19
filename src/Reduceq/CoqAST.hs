abadb3909b8d7a594c70c6c88b68124bc3188f7f
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2017-05-19 18:47:51 +0200

    Use uniplate to remove boring boilerplate

--- src/Reduceq/CoqAST.hs
+++ src/Reduceq/CoqAST.hs
@@ -13,7 +13,7 @@ import Reduceq.Prelude
 
 import Reduceq.AST (IntBinop(..), IntComp(..))
 
-import Control.Lens hiding (index, op)
+import Control.Lens hiding ((&), index, op)
 import Data.Data
 
 -- | DeBruijn indices
@@ -81,59 +81,33 @@ instance Data a => Plated (Expr a)
 
 -- | Increment free DeBruijn indices greater or equal than m by n
 liftVarsAbove :: Word -> Word -> Expr VarId -> Expr VarId
-liftVarsAbove m n e = go e
+liftVarsAbove m n expr = go expr
   where
     go (Var (VarId index))
       | index >= m = Var (VarId (index + n))
       | otherwise = Var (VarId index)
-    go (App x y) = App (go x) (go y)
     go (Abs ty body) = go (Abs ty (liftVarsAbove (succ m) n body))
-    go (Fst x) = Fst (go x)
-    go (Snd x) = Snd (go x)
-    go (Pair x y) = Pair (go x) (go y)
-    go (Inl x) = Inl (go x)
-    go (Inr x) = Inr (go x)
     go (Case c x y) =
       Case (go c) (liftVarsAbove (succ m) n x) (liftVarsAbove (succ m) n y)
-    go lit@(IntLit _) = lit
-    go (If cond x y) = If (go cond) (go x) (go y)
-    go (IntBinop op x y) = IntBinop op (go x) (go y)
-    go (IntComp comp x y) = IntComp comp (go x) (go y)
-    go Unit = Unit
-    go (Iter body init) = Iter (go body) (go init)
-    go (Set a i v) = Set (go a) (go i) (go v)
-    go (SetAtKey a k v) = Set (go a) (go k) (go v)
-    go (Read a i) = Read (go a) (go i)
-    go (ReadAtKey a k) = ReadAtKey (go a) (go k)
+    go e = e & plate %~ go
 
 shiftVars :: Expr VarId -> Expr VarId
 shiftVars = liftVarsAbove 0 1
 
 substAt :: VarId -> Expr VarId -> Expr VarId -> Expr VarId
-substAt id substitute e = go e
+substAt id substitute expr = go expr
   where
     go (Var id')
       | id == id' = substitute
       | id' > id = Var (pred id')
       | otherwise = Var id'
-    go (App x y) = App (go x) (go y)
     go (Abs ty body) = Abs ty (substAt (succ id) substitute body)
-    go (Fst x) = Fst (go x)
-    go (Snd x) = Snd (go x)
-    go (Pair x y) = Pair (go x) (go y)
-    go (Inl x) = Inl (go x)
-    go (Inr x) = Inr (go x)
     go (Case c x y) =
       Case
         (go c)
         (substAt (succ id) substitute x)
         (substAt (succ id) substitute y)
-    go (IntBinop op x y) = IntBinop op (go x) (go y)
-    go (IntComp comp x y) = IntComp comp (go x) (go y)
-    go lit@(IntLit _) = lit
-    go (If cond ifTrue ifFalse) = If (go cond) (go ifTrue) (go ifFalse)
-    go Unit = Unit
-    go (Iter body init) = Iter (go body) (go init)
+    go e = e & plate %~ go
 
 betaReduce :: Expr VarId -> Expr VarId
 betaReduce =
